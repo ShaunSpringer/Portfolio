05e948b19808a716e531a731775fcbf93b22407b
Shaun Springer <shaun.springer@gmail.com>
2012-03-21 20:53:23 -0400

    Nearly got map animation working, but shifting gears towards kinetic type animation

--- public/js/about.me.js
+++ public/js/about.me.js
@@ -1,6 +1,10 @@
 var map;
 
-$(document).ready(function(){	
+$(document).ready(function(){
+	
+	//shrink our header
+	$('header').animate({top: '-19em'}, 1500);
+	
 	//intialize the map
 	var mapOptions = {
   	maxZoom: 9,
@@ -9,7 +13,9 @@ $(document).ready(function(){
     center: new google.maps.LatLng(27, 30),
     mapTypeControl: false,
     streetViewControl: false,
-    disableDefaultUI: true
+    disableDefaultUI: true,
+    disableDoubleClickZoom: true,
+    draggable: false
 	};
  	
  	var mapType = new google.maps.StyledMapType([{
@@ -34,7 +40,7 @@ $(document).ready(function(){
   map.setMapTypeId('styledMapType'); 
   
   resizeMap();
-  setTimeout(startAnimating, 5000);
+  setTimeout(animateLocations, 2500);
 });
 
 //bind to the resize event so we can correct the map size
@@ -52,17 +58,120 @@ function resizeMap(){
 }
 
 //define our locations
+var currentIndex = 0;
 var locationData = [
   {
 		loc: new google.maps.LatLng(40.7774,  -73.4683),
-		title: "My Hometown"
+		zoom: 8,
+		title: "Birthplace - Plainview, NY",
+		date: "December 1984",
+		body: "Born to two wonderful parents, Jan and Peter."
+	},{
+		loc: new google.maps.LatLng(43.850251, -74.007511),
+		zoom: 8,
+		title: "Camp BACO - Minerva, NY",
+		date: "June 1994",
+		body: "Off to summer camp for the first time"
+	},{
+		loc: new google.maps.LatLng(42.894076, -78.880463),
+		zoom: 8,
+		title: "SUNY Buffalo - Buffalo, NY",
+		date: "August 2002",
+		body: "Began studies as a Computer Science major."
+	},{
+		loc: new google.maps.LatLng(42.894076, -78.880463),
+		zoom: 8,
+		title: "SUNY Buffalo - Buffalo, NY",
+		date: "February 2005",
+		body: "Changed majors to Electrical Engineering."
+	},{
+		loc: new google.maps.LatLng(-33.872660, 151.206937),
+		zoom: 8,
+		title: "University of Technology - Syndey, AUS",
+		date: "June 2005",
+		body: "Studied abroad at UTS."
+	},{
+		loc: new google.maps.LatLng(42.894076, -78.880463),
+		zoom: 8,
+		title: "Founded Gangstar Paintball, LLC",
+		date: "August 2006",
+		body: "Started career as an entrepreneur"
 	}
-]
+];
 
 /**
  * Starts our beautiful animations
  */
-function startAnimating(){
-	map.panTo(locationData[0].loc);
-	map.setZoom(4);
+var overlay;
+function animateLocations(){
+	
+	var data = locationData[currentIndex];
+	map.panTo(data.loc);
+	map.setZoom(data.zoom);
+	
+	//remove our current overlay if it exists
+	if (overlay)
+	{
+		overlay.setMap(null);
+	}
+	
+	overlay = new CustomMarker(data.loc, map);
+  var iw = new google.maps.InfoWindow({content: data.body, pixelOffset: new google.maps.Size(5,0)});
+  iw.open(map, overlay);	
+	
+	currentIndex++;
+  setTimeout(animateLocations, 5000);
 }
+
+/**
+ * Define our custom marker for the timeline
+ */
+	function CustomMarker(latlng, map) {
+    this.latlng_ = latlng;
+
+    // Once the LatLng and text are set, add the overlay to the map.  This will
+    // trigger a call to panes_changed which should in turn call draw.
+    this.setMap(map);
+  }
+
+  CustomMarker.prototype = new google.maps.OverlayView();
+
+  CustomMarker.prototype.draw = function() {
+    var me = this;
+
+    // Check if the div has been created.
+    var div = this.div_;
+    if (!div) {
+      // Create a overlay text DIV
+      div = this.div_ = $('div');
+      
+      // Create the DIV representing our CustomMarker
+      div.style.border = "none";
+      div.style.position = "absolute";
+      div.style.paddingLeft = "0px";
+      div.style.cursor = 'pointer';
+
+      // Then add the overlay to the DOM
+      var panes = this.getPanes();
+      panes.overlayImage.appendChild(div);
+    }
+
+    // Position the overlay 
+    var point = this.getProjection().fromLatLngToDivPixel(this.latlng_);
+    if (point) {
+      div.style.left = point.x + 'px';
+      div.style.top = point.y + 'px';
+    }
+  };
+
+  CustomMarker.prototype.remove = function() {
+    // Check if the overlay was on the map and needs to be removed.
+    if (this.div_) {
+      this.div_.parentNode.removeChild(this.div_);
+      this.div_ = null;
+    }
+  };
+
+  CustomMarker.prototype.getPosition = function() {
+   return this.latlng_;
+  };
---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('navigation#about').click(function(){
+		window.location = '/about/';
+	});
+	
+	$('navigation#resume').click(function(){
+		window.location = '/resume/';
+	});
+});
